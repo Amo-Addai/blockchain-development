163db10e577b77ad072d5110f3414de056dee523
Mr.Amo-Addai <kwadwoamoad@gmail.com>
2025-10-19 11:45:08 +0000

    .

--- golang-blockchain/0_go_blockchain/blockchain_server/blockchain_server.go
+++ golang-blockchain/0_go_blockchain/blockchain_server/blockchain_server.go
@@ -1,4 +1,42 @@
 
+// TODO: ...
+func Method() {
+	switch() {
+	case "":
+		
+		// todo: ..
+
+		if !t.Validate() {
+			log.Println("ERROR: missing field(s)")
+			io.WriteString(w, string(utils.JsonStatus("fail")))
+			return
+		}
+		publicKey := utils.PublicKeyFromString(*tSenderPublicKey)
+		signature := utils.SignatureFromString(*t.Signature)
+		bc := bcs.GetBlockchain()
+		isCreated :==bc.CreateTransaction(
+			*t.SenderBlockchainAddress,
+			*t.RecipientBlockchainAddress,
+			*t.Value,
+			publicKey,
+			signature
+		)
+		w.Header().Add("Content-Type", "application/json")
+		var m []byte
+		if !isCreated {
+			w.WriteHeader(http.StatusBadRequest)
+			m = utils.JsonStatus("fail")
+		} else {
+			w.WriteHeader(http.StatusCreated)
+			m = utils.JsonStatus("success")
+		}
+		io.WriteString(w, string(m))
+	default:
+		log.Println("ERROR: Invalid HTTP Method")
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
 func (bcs *BlockchainServer) Mine (w http.ResponseWriter, req *http.Request) {
 	switch req.Method {
 	case http.MethodGet:
